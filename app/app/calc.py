9b30745d72c87d5312bb45fa53c2c8577825a4e9
Pravin Kamble <iampbkamble@gmail.com>
2023-09-10 14:26:13 +0530

    Postgres database configuration added in the Dockerfile

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator function
 """
 
+
 def add(x, y):
     """ Add x and y and return result."""
     return x + y
+
 
 def sub(x, y):
     """ Substract x and y and return result."""
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,14 +6,14 @@ from django.test import SimpleTestCase
 
 from . import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
-
     def test_add_numbers(self):
         """Adding numbers together."""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-    
+
     def test_substract_numbers(self):
         """Subtracting numbers."""
         res = calc.sub(20, 10)
